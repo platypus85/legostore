1fa789ab9b1a1186905be68e88f09026cb9c1dd2
Alberto Camillo <info@albertocamillo.com>
2019-06-08 13:38:50 +1000

    Added querydsl

--- src/main/java/ac/legostore/api/LegoStoreController.java
+++ src/main/java/ac/legostore/api/LegoStoreController.java
@@ -2,7 +2,10 @@ package ac.legostore.api;
 
 import ac.legostore.model.LegoSet;
 import ac.legostore.model.LegoSetDifficulty;
+import ac.legostore.model.QLegoSet;
 import ac.legostore.persistence.LegoSetRepository;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,44 +27,57 @@ public class LegoStoreController {
     }
 
     @GetMapping("/all")
-    public Collection<LegoSet> getAll(){
+    public Collection<LegoSet> getAll() {
         Sort sortByThemeAsc = Sort.by("theme").ascending();
         return this.legoSetRepository.findAll(sortByThemeAsc);
     }
 
     @PutMapping
-    public void update(@RequestBody LegoSet legoSet){
+    public void update(@RequestBody LegoSet legoSet) {
         this.legoSetRepository.save(legoSet);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable String id){
+    public void delete(@PathVariable String id) {
         this.legoSetRepository.deleteById(id);
     }
 
     @GetMapping("/{id}")
-    public LegoSet byId(@PathVariable String id){
+    public LegoSet byId(@PathVariable String id) {
         return this.legoSetRepository.findById(id).orElse(null);
     }
 
     @GetMapping("/byTheme/{theme}")
-    public Collection<LegoSet> byTheme(@PathVariable String theme){
-        return this.legoSetRepository.findAllByThemeContains(theme);
+    public Collection<LegoSet> byTheme(@PathVariable String theme) {
+        Sort sortbyThemeAsc = Sort.by("theme").ascending();
+        return this.legoSetRepository.findAllByThemeContains(theme, sortbyThemeAsc);
     }
 
     @GetMapping("/hardThatStartsWithM")
-    public Collection<LegoSet> hardThatStartsWithM(){
+    public Collection<LegoSet> hardThatStartsWithM() {
         return this.legoSetRepository.findAllByDifficultyAndNameContains(LegoSetDifficulty.HARD, "M");
     }
 
     @GetMapping("/byDeliveryFeeLessThan/{price}")
-    public Collection<LegoSet> byDeliveryFeeLessThan(@PathVariable int price){
+    public Collection<LegoSet> byDeliveryFeeLessThan(@PathVariable int price) {
         return this.legoSetRepository.findAllByDeliveryFeeLessThan(price);
     }
 
     @GetMapping("/byGreatReviews")
-    public Collection<LegoSet> byGreatReviews(){
+    public Collection<LegoSet> byGreatReviews() {
         return this.legoSetRepository.findAllByGreatReviews();
+    }
+
+    @GetMapping("/bestBuys")
+    public Collection<LegoSet> bestBuys() {
+        QLegoSet query = new QLegoSet("query");
+        BooleanExpression inStockFilter = query.deliveryInfo.inStock.isTrue();
+        Predicate smallDeliveryFeeFilter = query.deliveryInfo.deliveryFee.lt(50);
+        Predicate hasGreatReviews = query.reviews.any().rating.eq(10);
+        
+        Predicate bestBuysFilter = inStockFilter.and(smallDeliveryFeeFilter).and(hasGreatReviews);
+
+        return (Collection<LegoSet>) this.legoSetRepository.findAll(bestBuysFilter);
     }
 
 }
--- src/main/java/ac/legostore/persistence/LegoSetRepository.java
+++ src/main/java/ac/legostore/persistence/LegoSetRepository.java
@@ -2,15 +2,17 @@ package ac.legostore.persistence;
 
 import ac.legostore.model.LegoSet;
 import ac.legostore.model.LegoSetDifficulty;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 
 @Repository
-public interface LegoSetRepository extends MongoRepository<LegoSet,String> {
-    Collection<LegoSet> findAllByThemeContains(String theme);
+public interface LegoSetRepository extends MongoRepository<LegoSet,String>, QuerydslPredicateExecutor<LegoSet> {
+    Collection<LegoSet> findAllByThemeContains(String theme, Sort sort);
     Collection<LegoSet> findAllByDifficultyAndNameContains(LegoSetDifficulty difficulty, String theme);
     @Query("{'delivery.deliveryFee':{$lt:?0}}")
     Collection<LegoSet> findAllByDeliveryFeeLessThan(int price);
